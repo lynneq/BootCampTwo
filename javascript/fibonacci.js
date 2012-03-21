468374eb3c4828ef2021b9707ad73fc5af324151
Lina Qu <lqu@thoughtworks.com>
2012-03-21 11:26:58 -0500

    problem2

---
+++ javascript/fibonacci.js
@@ -0,0 +1,27 @@
+function f(largestValue) {
+    var fibs = new Array();
+    fibs.push(1);
+    fibs.push(2);
+    var i=2;
+
+    while(true){
+        var tmp = fibs[i-1] + fibs[i-2];
+        if(tmp >= largestValue)
+            break;
+        fibs.push(tmp);
+        i++;
+    }
+    return fibs;
+}
+
+function getEvenFibs(fibs){
+   var i;
+   var sum=0;
+   for(i=0; i<fibs.length; i++){
+        if(fibs[i] %2 ==0){
+            sum+=fibs[i];
+        }
+   }
+   return sum;
+
+}
